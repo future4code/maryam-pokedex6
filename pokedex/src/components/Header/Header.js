5b6bc80e4a28d4f352c39a0fe52f51b08c919067
Milena Lara <milenalrf@gmail.com>
2021-10-18 18:55:29 -0300

    Transformei a logo em clicável que leva para home

--- pokedex/src/components/Header/Header.js
+++ pokedex/src/components/Header/Header.js
@@ -2,7 +2,7 @@ import React from "react";
 import { AppBar, Button, IconButton, Toolbar, Typography } from "@material-ui/core";
 import { Box } from "@material-ui/system";
 import { useHistory, useParams } from "react-router";
-import PokedexLogo from "../../assets/Pokedex.png"
+import PokedexLogo from "../../assets/Pokedex.png";
 import styled from "styled-components"
 
 const Logo = styled.img`
@@ -25,7 +25,11 @@ function Header() {
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position="static">
         <Toolbar>
-          <Logo src={PokedexLogo} />
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            <Button variant="text" onClick={() => goToHomePage()}>
+              <Logo src={PokedexLogo} />
+            </Button>
+          </Typography>
           <Button color="inherit" onClick={() => goToHomePage()}>Home</Button>
           <Button color="inherit" onClick={() => goToPokedexPage()}>Pokedex</Button>
         </Toolbar>
