0ded556519f8bf9a0a1ecfc7a9282e0f4e062e99
marina ramminger <ramminger.marina@gmail.com>
2021-10-18 17:29:35 -0300

    CardsPokeDetails, CardsHome, CardsPokedex

--- pokedex/src/pages/PokedexPage/PokedexPage.js
+++ pokedex/src/pages/PokedexPage/PokedexPage.js
@@ -1,10 +1,19 @@
 import React from 'react';
+import CardsPokedex from '../../components/CardsPokedex/CardsPokedex';
+import { Box } from '@material-ui/system';
 
 function PokedexPage() {
   return (
-    <div>
-      PokeDex
-    </div>
+    <Box sx={{ display: 'grid',rowGap: 4, gridTemplateColumns: 'repeat(4, 2fr)',  m: 4, p:2}}>
+    <CardsPokedex />
+    <CardsPokedex />
+    <CardsPokedex />
+    <CardsPokedex />
+    <CardsPokedex />
+    <CardsPokedex />
+    <CardsPokedex />
+    <CardsPokedex />
+  </Box>
   );
 }
 
