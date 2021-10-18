84e0e6474f43bf6dcb9bcd6cb03f6d23c20f23af
Milena Lara <milenalrf@gmail.com>
2021-10-18 18:51:11 -0300

    Adicionei imagem da logo no header

--- pokedex/src/components/Header/Header.js
+++ pokedex/src/components/Header/Header.js
@@ -2,6 +2,13 @@ import React from "react";
 import { AppBar, Button, IconButton, Toolbar, Typography } from "@material-ui/core";
 import { Box } from "@material-ui/system";
 import { useHistory, useParams } from "react-router";
+import PokedexLogo from "../../assets/Pokedex.png"
+import styled from "styled-components"
+
+const Logo = styled.img`
+  height: 3rem;
+  margin: 0.5rem;
+`
 
 function Header() {
   const history = useHistory();
@@ -18,9 +25,7 @@ function Header() {
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position="static">
         <Toolbar>
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            Header
-          </Typography>
+          <Logo src={PokedexLogo} />
           <Button color="inherit" onClick={() => goToHomePage()}>Home</Button>
           <Button color="inherit" onClick={() => goToPokedexPage()}>Pokedex</Button>
         </Toolbar>
