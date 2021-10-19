e326536c020dc5ff14ba1a0864a0c1f3525d6be0
Milena Lara <milenalrf@gmail.com>
2021-10-18 23:05:45 -0300

    Testei refatorar os cards e mudar as cores do tema

--- pokedex/src/pages/HomePage/HomePage.js
+++ pokedex/src/pages/HomePage/HomePage.js
@@ -1,10 +1,22 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import CardsHome from '../../components/CardsHome/CardsHome';
 import { Box } from '@material-ui/system';
+import Cards from "../../components/Cards/Cards"
 
 function HomePage() {
   return (
     <Box sx={{ display: 'grid', rowGap: 4, gridTemplateColumns: 'repeat(4, 2fr)', m: 4, p: 2 }}>
+      <Cards
+        cardWidth={250}
+        pokeName={"Charmander"}
+        pokeImage={"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/6.png"}
+        textBtn1={"adicionar"}
+        textBtn2={"detalhes"}
+        colorBtn1={"secondary"}
+        colorBtn2={"secondary"}
+        variantBtn1={"contained"}
+        variantBtn2={"contained"}
+      />
       <CardsHome />
       <CardsHome />
       <CardsHome />
--- pokedex/src/pages/PokedexPage/PokedexPage.js
+++ pokedex/src/pages/PokedexPage/PokedexPage.js
@@ -1,11 +1,23 @@
 import React from 'react';
 import CardsPokedex from '../../components/CardsPokedex/CardDetailsPo';
 import { Box } from '@material-ui/system';
+import Cards from "../../components/Cards/Cards"
 
 
 function PokedexPage() {
   return (
     <Box sx={{ display: 'grid', rowGap: 4, gridTemplateColumns: 'repeat(4, 2fr)', m: 4, p: 2 }}>
+      <Cards
+        cardWidth={250}
+        pokeName={"Charmander"}
+        pokeImage={"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/6.png"}
+        textBtn1={"remover"}
+        textBtn2={"detalhes"}
+        colorBtn1={"secondary"}
+        colorBtn2={"secondary"}
+        variantBtn1={"outlined"}
+        variantBtn2={"contained"}
+      />
       <CardsPokedex />
       <CardsPokedex />
       <CardsPokedex />
