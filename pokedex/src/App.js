c151c9473f73a27d3ca3c7a783b27ed3de5aa722
marina ramminger <ramminger.marina@gmail.com>
2021-10-20 12:38:38 -0300

    atualização pokedex

--- pokedex/src/App.js
+++ pokedex/src/App.js
@@ -2,12 +2,15 @@ import React, { useState } from 'react';
 import Router from "./router/Router";
 import { ThemeProvider } from '@material-ui/system';
 import { theme } from './theme';
+import PokedexPage from './pages/PokedexPage/PokedexPage';
 
 function App() {
   return (
+
     <ThemeProvider theme={theme}>
       < Router />
     </ThemeProvider >
+
   );
 }
 
